ef7f53e0f7b5a8dac9505319fe4faf446ccdbb9f
Brian Brietzke <bbrietzke@gmail.com>
2018-01-03 15:28:43 -0600

    Working on the card-ish interface.

--- src/components/Crew.vue
+++ src/components/Crew.vue
@@ -9,21 +9,17 @@
         ${{ currentStash }} funding expended
       </li>
     </ul>
-    <ul id='messaging' v-if='messagesAvailable'>
-      <li v-for="msg in messages">
-        {{msg}}
-      </li>
-      </li>
-        <button type="button" class="pure-button pure-button-primary" v-on:click="this.clearCurrentMessages">Clear Messages</button>
-      </li>
     </ul>
-    <ul id='crewList'>
+    <ul id='crewList' class='pure-g'>
       <li v-for='m in members'>
           <ul v-bind:id='m.id' class='member'>
-            <li><span class='alias'>{{ m.alias }}</span> ( {{ m.name }} )</li>
-            <li>{{ m.reputation }}</li>
-            <li>${{ m.funding }}</li>
-            <li><a href='' v-on:click.prevent='removeMember(m)'>remove</a></li>
+            <li class='headshot pure-u-1-1'>{{ m.id }}</li>
+            <li class='alias pure-u-1-2'>{{ m.alias }}</li>
+            <li class='name pure-u-1-2'>{{ m.name }}</li>
+            <li class='reputation pure-u-1-4'>&nbsp;</li>
+            <li class='reputation pure-u-1-4'>Reputation: {{ m.reputation }}</li>
+            <li class='funding pure-u-1-4'>Funding: ${{ m.funding }}</li>
+            <li class='actions pure-u-1-4'><a href='' v-on:click.prevent='removeMember(m)'>remove</a></li>
           </ul>
       </li>
     </ul>
@@ -41,11 +37,7 @@ export default {
     ])
   },
   computed: {
-    messagesAvailable: function () {
-      return this.messages.length > 0
-    },
     ...mapGetters({
-      messages: 'messages',
       members: 'members',
       currentReputation: 'currentReputation',
       currentStash: 'currentStash'
@@ -60,8 +52,31 @@ export default {
 <style scoped>
 #crew {
 }
-ul.crewList {
-
+ul#crewList {
+  list-style-type: none;
+  display: inline-block;
+}
+ul.member {
+  width: 100%;
+  display: inline-block;
+  list-style-type: none;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,.15);
+  margin: 5px;
+  padding: 5px;
+  transition: all .2s ease-in-out;
+}
+ul.member:hover {
+  /*box-shadow: 0 5px 22px 0 rgba(0,0,0,.25);*/
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  margin: 7px;
+}
+ul.member li.headshot {
+  float: left;
+  height: 80px;
+  width: 80px;
+}
+ul.member li.alias {
+  font-weight: bolder;
 }
 span.alias {
   font-weight: bolder;
